d1579b6c6f83fa4139183592e5018d85d9823e91
haalahibrahim03 <mdl19ebe025@mec.ac.in>
2023-08-22 11:10:28 +0530

    gatsby deploy

--- .cache/page-ssr/routes/component---src-templates-blog-post-js.js
+++ .cache/page-ssr/routes/component---src-templates-blog-post-js.js
@@ -17,7 +17,66 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_bio__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(9359);
 /* harmony import */ var _components_layout__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(8678);
 /* harmony import */ var _components_seo__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(9357);
-const BlogPostTemplate=({data:{previous,next,site,markdownRemark:post},location})=>{var _site$siteMetadata;const siteTitle=((_site$siteMetadata=site.siteMetadata)===null||_site$siteMetadata===void 0?void 0:_site$siteMetadata.title)||`Title`;return/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_layout__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z,{location:location,title:siteTitle},/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("article",{className:"blog-post",itemScope:true,itemType:"http://schema.org/Article"},/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("header",null,/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1",{itemProp:"headline"},post.frontmatter.title),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p",null,post.frontmatter.date)),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("section",{dangerouslySetInnerHTML:{__html:post.html},itemProp:"articleBody"}),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("hr",null),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("footer",null,/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_bio__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z,null))),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("nav",{className:"blog-post-nav"},/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("ul",{style:{display:`flex`,flexWrap:`wrap`,justifyContent:`space-between`,listStyle:`none`,padding:0}},/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li",null,previous&&/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(gatsby__WEBPACK_IMPORTED_MODULE_1__.Link,{to:previous.fields.slug,rel:"prev"},"\u2190 ",previous.frontmatter.title)),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li",null,next&&/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(gatsby__WEBPACK_IMPORTED_MODULE_1__.Link,{to:next.fields.slug,rel:"next"},next.frontmatter.title," \u2192")))));};const Head=({data:{markdownRemark:post}})=>{return/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_seo__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .Z,{title:post.frontmatter.title,description:post.frontmatter.description||post.excerpt});};/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (BlogPostTemplate);const pageQuery="3085459379";
+
+
+
+
+
+const BlogPostTemplate = ({
+  data: {
+    previous,
+    next,
+    site,
+    markdownRemark: post
+  },
+  location
+}) => {
+  var _site$siteMetadata;
+  const siteTitle = ((_site$siteMetadata = site.siteMetadata) === null || _site$siteMetadata === void 0 ? void 0 : _site$siteMetadata.title) || `Title`;
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_layout__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {
+    location: location,
+    title: siteTitle
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("article", {
+    className: "blog-post",
+    itemScope: true,
+    itemType: "http://schema.org/Article"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("header", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", {
+    itemProp: "headline"
+  }, post.frontmatter.title), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, post.frontmatter.date)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("section", {
+    dangerouslySetInnerHTML: {
+      __html: post.html
+    },
+    itemProp: "articleBody"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("hr", null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("footer", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_bio__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z, null))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("nav", {
+    className: "blog-post-nav"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("ul", {
+    style: {
+      display: `flex`,
+      flexWrap: `wrap`,
+      justifyContent: `space-between`,
+      listStyle: `none`,
+      padding: 0
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, previous && /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(gatsby__WEBPACK_IMPORTED_MODULE_1__.Link, {
+    to: previous.fields.slug,
+    rel: "prev"
+  }, "\u2190 ", previous.frontmatter.title)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, next && /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(gatsby__WEBPACK_IMPORTED_MODULE_1__.Link, {
+    to: next.fields.slug,
+    rel: "next"
+  }, next.frontmatter.title, " \u2192")))));
+};
+const Head = ({
+  data: {
+    markdownRemark: post
+  }
+}) => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_seo__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .Z, {
+    title: post.frontmatter.title,
+    description: post.frontmatter.description || post.excerpt
+  });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (BlogPostTemplate);
+const pageQuery = "3085459379";
 
 /***/ })
 
